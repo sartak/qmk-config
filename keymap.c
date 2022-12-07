312c819b4071cab95b8980d9be6e7a27cc4274de
Shawn M Moore <code@sartak.org>
2022-12-06 22:06:44 -0500

    Flesh out function layer (…I mean…)

--- keymap.c
+++ keymap.c
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [FUNCTION] = LAYOUT_split_3x5_2(
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-                        TO(ALPHA),  KC_NO,    KC_NO,  KC_NO
+    QK_BOOT,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  QK_BOOT,
+    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+                          TO(ALPHA),  KC_NO,    KC_NO,  KC_NO
     ),
 };

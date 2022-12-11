51958979bd2fc3b704f4aeebea4d2922559f2688
Shawn M Moore <code@sartak.org>
2022-12-11 16:37:41 -0500

    Add a layer of indirection in keymap

--- keymap.c
+++ keymap.c
@@ -16,11 +16,155 @@
 #define KC_MDASH LALT(LSFT(KC_MINUS))
 #define KC_ELLIPSIS LALT(KC_SEMICOLON)
 
+#define A_W TH(W)
+#define A_L TH(L)
+#define A_Y TH(Y)
+#define A_P TH(P)
+#define A_B TH(B)
+#define A_Z TH(Z)
+#define A_F TH(F)
+#define A_O TH(O)
+#define A_U TH(U)
+#define A_q TH(QUOTE)
+#define A_C TH(C)
+#define A_R TH(R)
+#define A_S TH(S)
+#define A_T TH(T)
+#define A_G TH(G)
+#define A_M TH(M)
+#define A_N TH(N)
+#define A_E TH(E)
+#define A_I TH(I)
+#define A_A TH(A)
+#define A_Q TH(Q)
+#define A_J TH(J)
+#define A_V TH(V)
+#define A_D TH(D)
+#define A_K TH(K)
+#define A_X TH(X)
+#define A_H TH(H)
+#define A_d TH(DOT)
+#define A_c TH(COMM)
+#define A_r TH(ENT)
+#define AT0  MO(SYMBOL)
+#define AT1  LSFT_T(KC_BSPC)
+#define AT2  LCTL_T(KC_SPC)
+#define AT3  MO(NUMBER)
+
+#define S_W KC_GRAVE
+#define S_L KC_TILDE
+#define S_Y KC_BACKSLASH
+#define S_P KC_LEFT_CURLY_BRACE
+#define S_B KC_DOLLAR
+#define S_Z KC_PERCENT
+#define S_F KC_RIGHT_CURLY_BRACE
+#define S_O KC_SLASH
+#define S_U KC_HASH
+#define S_q KC_QUOTE
+#define S_C KC_AMPERSAND
+#define S_R KC_ASTERISK
+#define S_S KC_EQUAL
+#define S_T KC_LEFT_PAREN
+#define S_G KC_LT
+#define S_M KC_GT
+#define S_N KC_RIGHT_PAREN
+#define S_E KC_MINUS
+#define S_I KC_PLUS
+#define S_A KC_PIPE
+#define S_Q KC_MDASH
+#define S_J KC_AT
+#define S_V KC_UNDERSCORE
+#define S_D KC_LEFT_BRACKET
+#define S_K KC_ELLIPSIS
+#define S_X KC_CIRCUMFLEX
+#define S_H KC_RIGHT_BRACKET
+#define S_d KC_DOT
+#define S_c KC_COMM
+#define S_r KC_ENT
+#define ST0 MO(ALPHA)
+#define ST1 LSFT_T(KC_BSPC)
+#define ST2 LCTL_T(KC_SPC)
+#define ST3 MO(NUMBER)
+
+#define N_W KC_BRIU
+#define N_L KC_6
+#define N_Y KC_5
+#define N_P KC_4
+#define N_B KC_SCREENSHOT
+#define N_Z KC_MUTE
+#define N_F KC_MPRV
+#define N_O KC_MPLY
+#define N_U KC_MNXT
+#define N_q KC_QUOTE
+#define N_C KC_LANGUAGE
+#define N_R KC_3
+#define N_S KC_2
+#define N_T KC_1
+#define N_G KC_0
+#define N_M KC_VOLU
+#define N_N KC_LEFT
+#define N_E KC_DOWN
+#define N_I KC_UP
+#define N_A KC_RIGHT
+#define N_Q KC_BRID
+#define N_J KC_9
+#define N_V KC_8
+#define N_D KC_7
+#define N_K KC_LOCKSCR
+#define N_X KC_VOLD
+#define N_H KC_ZOOMIN
+#define N_d KC_DOT
+#define N_c KC_COMM
+#define N_r KC_ENT
+#define NT0 MO(ALPHA)
+#define NT1 LSFT_T(KC_BSPC)
+#define NT2 LCTL_T(KC_SPC)
+#define NT3 MO(SYMBOL)
+
+#define F_W QK_BOOT
+#define F_L KC_NO
+#define F_Y KC_NO
+#define F_P KC_NO
+#define F_B QK_REBOOT
+#define F_Z QK_REBOOT
+#define F_F KC_NO
+#define F_O KC_NO
+#define F_U KC_NO
+#define F_q QK_BOOT
+#define F_C KC_NO
+#define F_R KC_NO
+#define F_S KC_NO
+#define F_T KC_NO
+#define F_G KC_NO
+#define F_M KC_NO
+#define F_N KC_NO
+#define F_E KC_NO
+#define F_I KC_NO
+#define F_A KC_NO
+#define F_Q KC_NO
+#define F_J KC_NO
+#define F_V KC_NO
+#define F_D KC_NO
+#define F_K KC_NO
+#define F_X KC_NO
+#define F_H KC_NO
+#define F_d KC_NO
+#define F_c KC_NO
+#define F_r KC_NO
+#define FT0 TO(ALPHA)
+#define FT1 KC_NO
+#define FT2 KC_NO
+#define FT3 KC_NO
+
 #include "chords.c"
 
 enum combo_events {
-  COMBO_LAYERS,
-  COMBO_DELETE_WORD,
+  ACOMBO_LAYERS,
+  SCOMBO_LAYERS,
+  NCOMBO_LAYERS,
+  ACOMBO_DELETE_WORD,
+  SCOMBO_DELETE_WORD,
+  NCOMBO_DELETE_WORD,
 
   CHORD_ENUM
 
@@ -28,12 +172,21 @@ enum combo_events {
 };
 uint16_t COMBO_LEN = _COMBO_LENGTH;
 
-const uint16_t PROGMEM combo_layers[] = {MO(SYMBOL), MO(NUMBER), COMBO_END};
-const uint16_t PROGMEM combo_delete_word[] = {LSFT_T(KC_BSPC), LCTL_T(KC_SPC), COMBO_END};
+const uint16_t PROGMEM acombo_layers[] = {AT0, AT3, COMBO_END};
+const uint16_t PROGMEM scombo_layers[] = {ST0, ST3, COMBO_END};
+const uint16_t PROGMEM ncombo_layers[] = {NT0, NT3, COMBO_END};
+const uint16_t PROGMEM acombo_delete_word[] = {AT1, AT2, COMBO_END};
+const uint16_t PROGMEM scombo_delete_word[] = {ST1, ST2, COMBO_END};
+const uint16_t PROGMEM ncombo_delete_word[] = {NT1, NT2, COMBO_END};
 
 combo_t key_combos[] = {
-  [COMBO_LAYERS] = COMBO(combo_layers, OSL(FUNCTION)),
-  [COMBO_DELETE_WORD] = COMBO(combo_delete_word, LALT(KC_BSPC)),
+  [ACOMBO_LAYERS] = COMBO(acombo_layers, OSL(FUNCTION)),
+  [SCOMBO_LAYERS] = COMBO(scombo_layers, OSL(FUNCTION)),
+  [NCOMBO_LAYERS] = COMBO(ncombo_layers, OSL(FUNCTION)),
+
+  [ACOMBO_DELETE_WORD] = COMBO(acombo_delete_word, LALT(KC_BSPC)),
+  [NCOMBO_DELETE_WORD] = COMBO(ncombo_delete_word, LALT(KC_BSPC)),
+  [SCOMBO_DELETE_WORD] = COMBO(scombo_delete_word, LALT(KC_BSPC)),
 
   CHORD_COMBOS
 };
@@ -46,31 +199,31 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [ALPHA] = LAYOUT_split_3x5_2(
-    TH(W),  TH(L),  TH(Y),  TH(P),  TH(B),    TH(Z),  TH(F),  TH(O),    TH(U),     TH(QUOTE),
-    TH(C),  TH(R),  TH(S),  TH(T),  TH(G),    TH(M),  TH(N),  TH(E),    TH(I),     TH(A),
-    TH(Q),  TH(J),  TH(V),  TH(D),  TH(K),    TH(X),  TH(H),  TH(DOT),  TH(COMM),  TH(ENT),
-             MO(SYMBOL),  LSFT_T(KC_BSPC),    LCTL_T(KC_SPC),  MO(NUMBER)
+      A_W, A_L, A_Y, A_P, A_B,  A_Z, A_F, A_O, A_U, A_q,
+      A_C, A_R, A_S, A_T, A_G,  A_M, A_N, A_E, A_I, A_A,
+      A_Q, A_J, A_V, A_D, A_K,  A_X, A_H, A_d, A_c, A_r,
+                     AT0, AT1,  AT2, AT3
     ),
 
     [SYMBOL] = LAYOUT_split_3x5_2(
-    KC_GRAVE,      KC_TILDE,     KC_BACKSLASH,   KC_LEFT_CURLY_BRACE,  KC_DOLLAR,      KC_PERCENT,     KC_RIGHT_CURLY_BRACE,  KC_SLASH,  KC_HASH,  KC_QUOTE,
-    KC_AMPERSAND,  KC_ASTERISK,  KC_EQUAL,       KC_LEFT_PAREN,        KC_LT,          KC_GT,          KC_RIGHT_PAREN,        KC_MINUS,  KC_PLUS,  KC_PIPE,
-    KC_MDASH,      KC_AT,        KC_UNDERSCORE,  KC_LEFT_BRACKET,      KC_ELLIPSIS,    KC_CIRCUMFLEX,  KC_RIGHT_BRACKET,      KC_DOT,    KC_COMM,  KC_ENT,
-                                                       MO(ALPHA),  LSFT_T(KC_BSPC),    LCTL_T(KC_SPC),  MO(NUMBER)
+      S_W, S_L, S_Y, S_P, S_B,  S_Z, S_F, S_O, S_U, S_q,
+      S_C, S_R, S_S, S_T, S_G,  S_M, S_N, S_E, S_I, S_A,
+      S_Q, S_J, S_V, S_D, S_K,  S_X, S_H, S_d, S_c, S_r,
+                     ST0, ST1,  ST2, ST3
     ),
 
     [NUMBER] = LAYOUT_split_3x5_2(
-    KC_BRIU,      KC_6,  KC_5,  KC_4,  KC_SCREENSHOT,    KC_MUTE,  KC_MPRV,    KC_MPLY,  KC_MNXT,  KC_QUOTE,
-    KC_LANGUAGE,  KC_3,  KC_2,  KC_1,  KC_0,             KC_VOLU,  KC_LEFT,    KC_DOWN,  KC_UP,    KC_RIGHT,
-    KC_BRID,      KC_9,  KC_8,  KC_7,  KC_LOCKSCR,       KC_VOLD,  KC_ZOOMIN,  KC_DOT,   KC_COMM,  KC_ENT,
-                         MO(ALPHA),  LSFT_T(KC_BSPC),    LCTL_T(KC_SPC),  MO(SYMBOL)
+      N_W, N_L, N_Y, N_P, N_B,  N_Z, N_F, N_O, N_U, N_q,
+      N_C, N_R, N_S, N_T, N_G,  N_M, N_N, N_E, N_I, N_A,
+      N_Q, N_J, N_V, N_D, N_K,  N_X, N_H, N_d, N_c, N_r,
+                     NT0, NT1,  NT2, NT3
     ),
 
     [FUNCTION] = LAYOUT_split_3x5_2(
-    QK_BOOT,  KC_NO,  KC_NO,  KC_NO,  QK_REBOOT,    QK_REBOOT,  KC_NO,  KC_NO,  KC_NO,  QK_BOOT,
-    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,        KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,
-    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,        KC_NO,      KC_NO,  KC_NO,  KC_NO,  KC_NO,
-                          TO(ALPHA),  KC_NO,        KC_NO,      KC_NO
+      F_W, F_L, F_Y, F_P, F_B,  F_Z, F_F, F_O, F_U, F_q,
+      F_C, F_R, F_S, F_T, F_G,  F_M, F_N, F_E, F_I, F_A,
+      F_Q, F_J, F_V, F_D, F_K,  F_X, F_H, F_d, F_c, F_r,
+                     FT0, FT1,  FT2, FT3
     ),
 };
 
